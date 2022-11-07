cf4fb79296cda8c7f032472eaf811bb28095eabb
Michelle Madeira <michellemadeira1@gmail.com>
2022-11-07 17:43:19 -0300

    new packge

---
+++ primeiro-exemplo/src/main/java/Config/DatasourceConfig.java
@@ -0,0 +1,21 @@
+package Config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatasourceConfig {
+
+	@Bean
+	public DataSource datasource() {
+        return DataSourceBuilder.create()
+          .driverClassName("com.mysql.cj.jdbc.Driver")
+          .url("jdbc:mysql://localhost:3306/mercado")
+          .username("root")
+          .password("root")
+          .build();	
+    }
+}
